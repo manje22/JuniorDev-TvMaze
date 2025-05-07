b8475a8ae9974fdec713224f9ff56dce8f56e63f
manje22 <uvodicmariela@gmail.com>
2025-05-07 17:14:19 +0200

    modified nextConfig to accept api images

--- tv_maze_jd/next.config.ts
+++ tv_maze_jd/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/api.tvmaze/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.tvmaze.com",
+        port: "",
+        pathname: "/uploads/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
