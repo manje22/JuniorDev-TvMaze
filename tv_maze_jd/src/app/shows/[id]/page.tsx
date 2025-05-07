e2a305717a63c1e56bbf494ad3f955cc1b7f4a88
manje22 <uvodicmariela@gmail.com>
2025-05-07 17:14:54 +0200

    made basic structure for showDetails page

--- tv_maze_jd/src/app/shows/[id]/page.tsx
+++ tv_maze_jd/src/app/shows/[id]/page.tsx
@@ -1,23 +1,65 @@
-
 import { error } from "console";
 import { notFound } from "next/navigation";
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function ShowDetails({ params }) {
   const { id } = await params;
   console.log("Id je: ", id);
 
-  const showRes = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const showRes = await fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`);
 
-  if(!showRes.ok){
+  if (!showRes.ok) {
     throw new Error("Show not found");
   }
 
   const showData = await showRes.json();
+  const poster = showData.image.original;
+  const cast = showData._embedded.cast;
+  console.log(cast);
 
-    return(
-        <div>
-            {showData.name}
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-center text-4xl mt-10">{showData.name}</h1>
+      <div>
+        <Image
+          src={poster}
+          alt={`${showData.name} poster`}
+          width={500}
+          height={500}
+          priority={true}
+        ></Image>
+      </div>
+      <div>
+        <ul>
+          <li>Summary: {showData.summary}</li>
+          <li>Premiered: {showData.premiered}</li>
+          <li>Ended: {showData.ended}</li>
+          <li>
+            Genres:
+            <div>
+              {showData.genres.map((g) => (
+                <p>{g}</p>
+              ))}
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>Cast</p>
+        {
+          cast.map((c) => (
+            <div>
+              <Image src={c.person.image.original} alt={c.person.name} width={100} height={100}></Image>
+              <p>{c.person.name}</p>
+              <p>plays: {c.character.name}</p>
+            </div>
+          ))
+        }
+      </div>
+      <div>
+        <Link href={`/shows/${id}/episodes`}>Click here to view episodes</Link>
+      </div>
+    </div>
+  );
 }
