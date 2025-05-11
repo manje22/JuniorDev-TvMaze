90ebfd9e81e49905afd3df87564f04cb4dca1365
manje22 <uvodicmariela@gmail.com>
2025-05-11 22:16:27 +0200

    created EpisodeDetails page

---
+++ tv_maze_jd/src/app/episodes/[id]/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+type Props ={
+    params: {id:string};
+};
+
+
+export default async function EpisodeDetails({params}: Props) {
+    const epId = params.id;
+    console.log(epId,"hello from episode detail page");
+    
+
+    const EpisodeDetails = await fetch(`https://api.tvmaze.com/episodes/${epId}`);
+
+    if (!EpisodeDetails.ok) {
+        throw new Error("Episode details not found");
+    }
+
+    const episodeData = await EpisodeDetails.json();
+    console.log(episodeData);
+
+    return(
+        <div>
+            <div>
+                <Image src={episodeData.image.original} width={500} height={300} alt="image"></Image>
+            </div>
+            <div>
+                <p>Name: {episodeData.name}</p>
+                <p>Season {episodeData.season} ep. {episodeData.number}</p>
+                <p>Summary: {episodeData.summary}</p>
+                <p>Rating: {episodeData.rating.average}</p>
+                <p>Airdate: {episodeData.airdate}</p>
+            </div>
+        </div>
+    )
+}
