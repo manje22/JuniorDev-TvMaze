054e1faec94528ca400b387837ba9c567a2feab9
manje22 <uvodicmariela@gmail.com>
2025-05-13 23:15:33 +0200

    created api

---
+++ tv_maze_jd/src/app/api/favorites/route.js
@@ -0,0 +1,15 @@
+let favorites = []; // demo “baza” u RAM-u
+ 
+export async function GET() {
+  return Response.json({ favorites });
+}
+ 
+export async function POST(request) {
+  const body = await request.json();
+  if (!body?.id)
+    return Response.json({ error: "id missing" }, { status: 400 });
+ 
+  if (!favorites.includes(body.id)) favorites.push(body.id);
+ 
+  return Response.json({ ok: true, favorites });
+}
