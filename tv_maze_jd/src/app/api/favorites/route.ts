9de73dd54547594c27cd4a93b976dd3877e72a3d
manje22 <uvodicmariela@gmail.com>
2025-05-15 10:39:08 +0200

    fixed delete logic

--- tv_maze_jd/src/app/api/favorites/route.ts
+++ tv_maze_jd/src/app/api/favorites/route.ts
@@ -1,10 +1,30 @@
 import { NextResponse } from "next/server";
-import { insertNewShow, getShowbyId, getShows, } from "../../db/statements";
+import { insertNewShow, getShowbyId, getShows, deleteShow, } from "../../db/statements";
+import { error } from "console";
 
  
 export async function GET() {
   const shows = getShows();
   return NextResponse.json(shows);
+}
+
+export async function DELETE(request: Request){
+  const body = await request.json();
+  console.log("From delete ", body);
+  const id = Number(body.tvmaze_id);
+  console.log("What im sending for id: ", id);
+
+  if (!id) {
+    return NextResponse.json({error: "Id not provided"}, {status:400});
+  }
+
+  try {
+    deleteShow(id);
+    return NextResponse.json({message: "Show deleted from favorites"});
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({error: "Error while deleting show"}, {status:500});
+  }
 }
 
  
