43923a3bd0ede37eea79c912124804112b946f6c
manje22 <uvodicmariela@gmail.com>
2025-05-19 20:54:27 +0200

    modified searchbar to use debounce

--- tv_maze_jd/components/SearchBar.tsx
+++ tv_maze_jd/components/SearchBar.tsx
@@ -1,41 +1,42 @@
 "use client"
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { useDebounce } from "@uidotdev/usehooks";
 
 export default function SearchBar() {
 
   const [activeSearch, setActiveSearch] = useState([]);
+  const debouncedSearchTerm = useDebounce(activeSearch, 300);
+  const [results, setResults] = useState([]);
 
-  const handleSearch = (e) => {
-    if (e.target.value == "") {
-      setActiveSearch([]);
-      return false;
-    }
-    console.log(e.target.value);
-    fetch(`https://api.tvmaze.com/search/shows?q=${e.target.value}`)
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      fetch(`https://api.tvmaze.com/search/shows?q=${activeSearch}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setActiveSearch(data);
+        setResults(data);
       })
       .catch((err) => console.error(err));
-  }
+    }
+  }, [debouncedSearchTerm])
+
 
   return (
     <form className="w-[500px] relative text-white">
       <div className="relative">
-        <input type="search" placeholder="Search..." className="w-full p-4 rounded-full bg-slate-800" onChange={(e) => handleSearch(e)}/>
+        <input type="search" placeholder="Search..." className="w-full p-4 rounded-full bg-slate-800" onChange={(e) => setActiveSearch(e.target.value)}/>
         <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-900 rounded-full">
           <AiOutlineSearch/>
         </button>
       </div>
 
       {
-        activeSearch.length > 0 && (
+        results.length > 0 && (
           <div className="absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2">
             {
-              activeSearch.map((s) => (
+              results.map((s) => (
                 <Link href={`/shows/${s.show.id}`} key={s.show.id}>
                   {s.show.name}
                 </Link>
