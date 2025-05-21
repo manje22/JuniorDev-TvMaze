29bbc79a8e57d43e2246a1528b7577bdbf64557d
manje22 <uvodicmariela@gmail.com>
2025-05-21 22:44:24 +0200

    created SessionContext

---
+++ tv_maze_jd/src/context/SessionContext.tsx
@@ -0,0 +1,28 @@
+// /context/SessionContext.tsx
+
+"use client";
+
+import { createContext, useContext } from "react";
+import { Session } from "next-auth";
+import { ReactNode } from "react";
+
+const SessionContext = createContext<Session | null>(null);
+
+export const useSessionContext = () => useContext(SessionContext);
+
+export const SessionProvider = ({
+  children,
+  value,
+}: {
+  children: ReactNode;
+  value: Session | null;
+}) => {
+  return (
+    <SessionContext.Provider value={value}>
+      {children}
+    </SessionContext.Provider>
+  );
+};
+
+export default SessionContext;
+
