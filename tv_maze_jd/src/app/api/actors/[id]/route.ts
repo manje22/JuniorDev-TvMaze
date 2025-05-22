c62a154dd4b839179c520abd28b887763b3c08a2
manje22 <uvodicmariela@gmail.com>
2025-05-22 15:09:28 +0200

    added route for checking if actor already in fav

---
+++ tv_maze_jd/src/app/api/actors/[id]/route.ts
@@ -0,0 +1,30 @@
+import { getActorbyId } from "@/app/db/statements";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = Number(params.id);
+
+  const url = new URL(req.url);
+  const mail = url.searchParams.get("user_mail");
+
+  if (!mail) {
+    return new Response(
+      JSON.stringify({ message: "Email not provided", status: 400 }),
+      { status: 400 }
+    );
+  }
+
+  const show = getActorbyId(id, mail);
+
+  if (!show) {
+    return new Response(
+      JSON.stringify({ message: "Actor not in favorites", status: 404 }),
+      { status: 404 }
+    );
+  }
+
+  return new Response(JSON.stringify(show), { status: 200 });
+}
