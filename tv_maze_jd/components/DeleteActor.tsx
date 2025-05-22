9ed7164a16e18dc4c54f341bec6e83e2513c1ea5
manje22 <uvodicmariela@gmail.com>
2025-05-22 15:56:00 +0200

    created button for removing actors from favs

---
+++ tv_maze_jd/components/DeleteActor.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useTransition, useEffect } from "react";
+import { useSessionContext } from "@/context/SessionContext";
+
+
+
+export default function DeleteActor({id, OnDelete}:{id:string; OnDelete:()=> void;}) {
+  const [isPending, startTransition] = useTransition();
+  const session = useSessionContext();
+
+
+
+  async function removeFavorite() {
+    const res = await fetch("/api/actors", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        tvmaze_id: id,
+        user_mail: session?.user?.email,
+      }),
+    });
+
+    const data = await res.json();
+    console.log("Deleted data: ",data);
+    OnDelete();
+  }
+
+  return (
+    <button
+      onClick={removeFavorite}
+      className="bg-red-600 text-white hover:bg-red-100"
+    >
+        Remove from favorites
+    </button>
+  );
+}
