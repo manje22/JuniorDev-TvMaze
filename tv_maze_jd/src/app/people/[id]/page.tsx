81f72a5dff571d57f352b53099889182b7ca996a
manje22 <uvodicmariela@gmail.com>
2025-05-22 15:57:20 +0200

    can add actor to favorites

--- tv_maze_jd/src/app/people/[id]/page.tsx
+++ tv_maze_jd/src/app/people/[id]/page.tsx
@@ -1,41 +1,67 @@
 import Image from "next/image";
 import NotFound from "./not-found";
+import ActorFavorite from "../../../../components/ActorFavorite";
+import { useSessionContext } from "@/context/SessionContext";
+import notFound from "./not-found";
+import { auth } from "@/app/auth";
 
-type Props ={
-    params: {id:string};
+type Props = {
+  params: { id: string };
 };
 
+export default async function ActorDetails({ params }: Props) {
+  const id = parseInt(params.id, 10);
+  console.log(id, "hello from actor detail page");
 
-export default async function ActorDetails({params}: Props) {
-    const actorId = params.id;
-    console.log(actorId,"hello from actor detail page");
-    
+  const session = await auth();
+  console.log("session from ActorDisplay: ", session);
 
-    const actorDetails = await fetch(`https://api.tvmaze.com/people/${actorId}?embed=castcredits`);
+  const actorRes = await fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`);
 
-    if (!actorDetails.ok) {
-        return NotFound();
-    }
+  if (!actorRes.ok) {
+    return notFound();
+  }
 
-    const actorData = await actorDetails.json();
-    console.log(actorData);
+  const actorDetails = await fetch(
+    `https://api.tvmaze.com/people/${id}?embed=castcredits`
+  );
 
-    return(
+  if (!actorDetails.ok) {
+    return NotFound();
+  }
+
+  const actorData = await actorDetails.json();
+  const actorFav = {
+    tvmaze_id: actorData.id,
+    user_mail: session?.user?.email,
+    name: actorData.name,
+    image: actorData.image?.medium,
+  };
+  console.log(actorData);
+
+  return (
+    <div>
+      <div>
+        <Image
+          src={actorData.image.original}
+          width={500}
+          height={300}
+          alt="image"
+        ></Image>
+      </div>
+      <div>
+        <p>Name: {actorData.name}</p>
+        <p>County: {actorData.country?.name || "n/a"}</p>
+        <p>Birthday: {actorData.birthday}</p>
+      </div>
+      <div>
+        <p>Appears in:</p>
         <div>
-            <div>
-                <Image src={actorData.image.original} width={500} height={300} alt="image"></Image>
-            </div>
-            <div>
-                <p>Name: {actorData.name}</p>
-                <p>County: {actorData.country?.name || "n/a"}</p>
-                <p>Birthday: {actorData.birthday}</p>
-            </div>
-            <div>
-                <p>Appears in:</p>
-                <div>
-                    nek ovde idu baren imena serija i imena lika kojeg su glumili, dodat link na str sa page info?
-                </div>
-            </div>
+          nek ovde idu baren imena serija i imena lika kojeg su glumili, dodat
+          link na str sa page info?
         </div>
-    )
+      </div>
+      <ActorFavorite actor={actorFav} initialSaved={false} />
+    </div>
+  );
 }
