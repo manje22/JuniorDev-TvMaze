04db50a399de8146b776701226f0e4f70037f8e9
manje22 <uvodicmariela@gmail.com>
2025-05-25 14:40:02 +0200

    added layout to people route to enable metadata addition

---
+++ tv_maze_jd/src/app/people/layout.tsx
@@ -0,0 +1,23 @@
+import { MyProps } from "@/types";
+
+export async function generateMetadata() {
+ 
+  return {
+    title: `Tv encyclopedia │ Actors`,
+    description: `Browse actors`,
+  };
+}
+
+
+export default async function PeopleLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <div className="flex">
+      <main className="flex-1 p-4">{children}</main>
+    </div>
+  );
+}
