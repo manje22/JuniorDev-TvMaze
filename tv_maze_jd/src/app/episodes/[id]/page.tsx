456b19f4f297b924114014a62041d24dca741cc4
manje22 <uvodicmariela@gmail.com>
2025-05-25 19:10:43 +0200

    modified comp to be stricter-cant deploy

--- tv_maze_jd/src/app/episodes/[id]/page.tsx
+++ tv_maze_jd/src/app/episodes/[id]/page.tsx
@@ -1,24 +1,30 @@
+import { notFound } from "next/navigation";
 import Image from "next/image";
-import { notFound } from "next/navigation"; 
 
+type EpisodePageProps = {
+  params: {
+    id: string;
+  };
+};
 
-export default async function EpisodeDetails({ params }: { params: { id: string } }) {
+export default async function EpisodeDetails({ params }: EpisodePageProps) {
   const epId = params.id;
-  const EpisodeDetails = await fetch(`https://api.tvmaze.com/episodes/${epId}`);
 
-  if (!EpisodeDetails.ok) {
+  const response = await fetch(`https://api.tvmaze.com/episodes/${epId}`);
+
+  if (!response.ok) {
     return notFound();
   }
 
-  const episodeData = await EpisodeDetails.json();
+  const res = await response.json();
 
   return (
     <div className="flex flex-col gap-5 ml-10">
       <h1 className="text-4xl font-bold p-5">
-        Season {episodeData.season} ep. {episodeData.number}
+        Season {res.season} ep. {res.number}
       </h1>
       <div>
-        <Image src={episodeData.image.original} width={500} height={300} alt="image" />
+        <Image src={res.image.original} width={500} height={300} alt="image" />
       </div>
       <div>
         <h2 className="text-4xl">Details:</h2>
@@ -26,19 +32,19 @@ export default async function EpisodeDetails({ params }: { params: { id: string 
       <div className="text-2xl flex flex-col gap-2">
         <p>
           <strong>Name: </strong>
-          {episodeData.name}
+          {res.name}
         </p>
         <p>
           <strong>Summary: </strong>
-          <span dangerouslySetInnerHTML={{ __html: episodeData.summary }} />
+          <span dangerouslySetInnerHTML={{ __html: res.summary }} />
         </p>
         <p>
           <strong>Rating: </strong>
-          {episodeData.rating.average}
+          {res.rating.average}
         </p>
         <p>
           <strong>Airdate: </strong>
-          {episodeData.airdate}
+          {res.airdate}
         </p>
       </div>
     </div>
