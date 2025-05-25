cce55d9e259a7e17580963f0d544223c04e20903
manje22 <uvodicmariela@gmail.com>
2025-05-25 14:43:17 +0200

    removed console.logs

--- tv_maze_jd/components/ActorFavorite.tsx
+++ tv_maze_jd/components/ActorFavorite.tsx
@@ -2,15 +2,10 @@
 
 import { useState, useTransition, useEffect } from "react";
 import { useSessionContext } from "@/context/SessionContext";
-
-type ActorDbEntity = {
-  tvmaze_id: number,
-  name: string,
-  image:string,
-}
+import { ActorDb } from "@/types";
 //ovo exporttat u types
 
-export default function ActorFavorite({actor,initialSaved=false}:{actor: ActorDbEntity, initialSaved:boolean}) {
+export default function ActorFavorite({actor,initialSaved=false}:{actor: ActorDb, initialSaved:boolean}) {
   const [saved, setSaved] = useState(initialSaved);
   const [isPending, startTransition] = useTransition();
   const [provjera, setProvjera] = useState(true);
@@ -23,11 +18,9 @@ export default function ActorFavorite({actor,initialSaved=false}:{actor: ActorDb
         setProvjera(false);
         return;
       }
-      console.log("Actor from favbtn: ",actor);
       fetch(`/api/actors/${id}?user_mail=${user_email}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log("Actor saved?: ", data);
         if (data.message === "Actor not in favorites") {
           setSaved(false)
         }
@@ -47,7 +40,6 @@ export default function ActorFavorite({actor,initialSaved=false}:{actor: ActorDb
       });
       
       const data = await res.json();
-      console.log("Add favorite actor message: ", data.message);
       setSaved(true);
     });
   }
