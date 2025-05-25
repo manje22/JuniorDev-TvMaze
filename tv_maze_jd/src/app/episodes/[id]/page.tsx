d07fca78295c03f960f72b55af9354069966abe4
manje22 <uvodicmariela@gmail.com>
2025-05-25 18:54:13 +0200

    removed my props

--- tv_maze_jd/src/app/episodes/[id]/page.tsx
+++ tv_maze_jd/src/app/episodes/[id]/page.tsx
@@ -1,51 +1,62 @@
 import Image from "next/image";
 import NotFound from "./not-found";
-import { MyProps } from "@/types";
 
-export async function generateMetadata({ params }: MyProps) {
-  const  id  = parseInt(params.id, 10);
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const id = parseInt(params.id, 10);
   const res = await fetch(`https://api.tvmaze.com/episodes/${id}`);
   if (!res.ok) return { title: "Episode not found" };
- 
+
   const data = await res.json();
- 
-  const image = data.image.medium;
- 
+  const image = data.image?.medium ?? "";
+
   return {
     title: `Tv encyclopedia episode │ ${data.name}`,
     description: `${data.name} details`,
     openGraph: {
       images: [{ url: image, width: 400, height: 400 }],
-    }
+    },
   };
 }
 
-export default async function EpisodeDetails({params}: MyProps) {
-    const epId = params.id;
+export default async function EpisodeDetails({ params }: { params: { id: string } }) {
+  const epId = params.id;
+  const EpisodeDetails = await fetch(`https://api.tvmaze.com/episodes/${epId}`);
 
-    const EpisodeDetails = await fetch(`https://api.tvmaze.com/episodes/${epId}`);
+  if (!EpisodeDetails.ok) {
+    return NotFound();
+  }
 
-    if (!EpisodeDetails.ok) {
-        return NotFound();
-    }
-    
-    const episodeData = await EpisodeDetails.json();
+  const episodeData = await EpisodeDetails.json();
 
-    return(
-        <div className="flex flex-col gap-5 ml-10">
-            <h1 className="text-4xl font-bold p-5">Season {episodeData.season} ep. {episodeData.number}</h1>
-            <div>
-                <Image src={episodeData.image.original} width={500} height={300} alt="image"></Image>
-            </div>
-            <div>
-                <h2 className="text-4xl">Details:</h2>
-            </div>
-            <div className="text-2xl flex flex-col gap-2">
-                <p><strong>Name: </strong>{episodeData.name}</p>
-                <p><strong>Summary: </strong> {episodeData.summary}</p>
-                <p><strong>Rating: </strong> {episodeData.rating.average}</p>
-                <p><strong>Airdate: </strong> {episodeData.airdate}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-5 ml-10">
+      <h1 className="text-4xl font-bold p-5">
+        Season {episodeData.season} ep. {episodeData.number}
+      </h1>
+      <div>
+        <Image src={episodeData.image.original} width={500} height={300} alt="image" />
+      </div>
+      <div>
+        <h2 className="text-4xl">Details:</h2>
+      </div>
+      <div className="text-2xl flex flex-col gap-2">
+        <p>
+          <strong>Name: </strong>
+          {episodeData.name}
+        </p>
+        <p>
+          <strong>Summary: </strong>
+          <span dangerouslySetInnerHTML={{ __html: episodeData.summary }} />
+        </p>
+        <p>
+          <strong>Rating: </strong>
+          {episodeData.rating.average}
+        </p>
+        <p>
+          <strong>Airdate: </strong>
+          {episodeData.airdate}
+        </p>
+      </div>
+    </div>
+  );
 }
